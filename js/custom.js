0c10f64612475690c28415f553ddab675e858e34
supriyofrontend <supriyo.codearts@gmail.com>
2022-12-22 13:43:29 +0530

    header structure added

--- js/custom.js
+++ js/custom.js
@@ -78,3 +78,8 @@ jQuery(document).ready(function() {
     });
     });
 
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
